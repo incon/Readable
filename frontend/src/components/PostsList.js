bec5affc74c7f9a029978ef0cce84a1f6f38460b
David Darrell <david.darrell@gmail.com>
2017-11-26 11:19:09 +0800

    Add sorting feature

--- frontend/src/components/PostsList.js
+++ frontend/src/components/PostsList.js
@@ -6,6 +6,13 @@ import { format } from "date-fns";
 import "./PostsList.css";
 
 class PostsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sortBy: "SCORE_DESC"
+    };
+  }
+
   componentWillMount() {
     this.props.fetchPosts();
   }
@@ -18,11 +25,53 @@ class PostsList extends Component {
     }
 
     if (category === "all") {
-      return posts;
+      return posts.sort(this.sortBy);
     }
 
-    return posts.filter(post => post.category === category);
+    return posts.filter(post => post.category === category).sort(this.sortBy);
   }
+
+  sortByScore(a, b) {
+    if (a.voteScore < b.voteScore) {
+      return -1;
+    }
+
+    if (a.voteScore > b.voteScore) {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  sortByDate(a, b) {
+    if (a.timestamp < b.timestamp) {
+      return -1;
+    }
+
+    if (a.timestamp > b.timestamp) {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  sortBy = (a, b) => {
+    if (this.state.sortBy === "SCORE_ASC") {
+      return this.sortByScore(a, b);
+    }
+
+    if (this.state.sortBy === "SCORE_DESC") {
+      return this.sortByScore(b, a);
+    }
+
+    if (this.state.sortBy === "DATE_ASC") {
+      return this.sortByDate(a, b);
+    }
+
+    if (this.state.sortBy === "DATE_DESC") {
+      return this.sortByDate(b, a);
+    }
+  };
 
   render() {
     const posts = this.posts();
@@ -30,6 +79,44 @@ class PostsList extends Component {
     return (
       <div className="post-list">
         <div className="post-list-actions">
+          <span className="sort-item">
+            <span className="sort-label">Score</span>
+            <button
+              className={`sort-button ${
+                this.state.sortBy === "SCORE_ASC" ? "sort-button-current" : ""
+              }`}
+              onClick={() => this.setState({ sortBy: "SCORE_ASC" })}
+            >
+              Low - High
+            </button>
+            <button
+              className={`sort-button ${
+                this.state.sortBy === "SCORE_DESC" ? "sort-button-current" : ""
+              }`}
+              onClick={() => this.setState({ sortBy: "SCORE_DESC" })}
+            >
+              High - Low
+            </button>
+          </span>
+          <span className="sort-item">
+            <span className="sort-label">Date</span>
+            <button
+              className={`sort-button ${
+                this.state.sortBy === "DATE_ASC" ? "sort-button-current" : ""
+              }`}
+              onClick={() => this.setState({ sortBy: "DATE_ASC" })}
+            >
+              Old - New
+            </button>
+            <button
+              className={`sort-button ${
+                this.state.sortBy === "DATE_DESC" ? "sort-button-current" : ""
+              }`}
+              onClick={() => this.setState({ sortBy: "DATE_DESC" })}
+            >
+              New - Old
+            </button>
+          </span>
           <Link to="/posts/create">
             <button>New</button>
           </Link>
@@ -38,6 +125,7 @@ class PostsList extends Component {
           posts.map(post => (
             <div className="post-list-item" key={post.id}>
               <div className="post-list-item-title">{post.title}</div>
+              <div className="post-list-item-score">Score {post.voteScore}</div>
               <div className="post-list-item-body">{post.body}</div>
               <div>
                 <span className="post-list-item-category">{post.category}</span>
