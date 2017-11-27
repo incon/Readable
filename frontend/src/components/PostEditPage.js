f3f1b106271be76df596870b865fc8a52cd39ff2
David Darrell <david.darrell@gmail.com>
2017-11-27 17:12:50 +0800

    Update edit post styling

--- frontend/src/components/PostEditPage.js
+++ frontend/src/components/PostEditPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import PostEdit from "./PostEdit";
+import { Link } from "react-router-dom";
 
 class PostEditPage extends Component {
   render() {
     return (
       <div>
-        Post Edit Page
+        <Link to="/">Home</Link>
+        <h2>Post Edit</h2>
         <PostEdit
           postId={this.props.match.params.postId}
           history={this.props.history}
