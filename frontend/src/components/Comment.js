f4d8c35441bc9b4f0f67fe3d7c27ae05e6bc862a
David Darrell <david.darrell@gmail.com>
2017-11-27 17:01:15 +0800

    Delete comments

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import CommentEdit from "./CommentEdit";
+import { connect } from "react-redux";
+import { fetchPostComments } from "../actions/index";
 
 class Comment extends Component {
   constructor(props) {
@@ -10,6 +12,19 @@ class Comment extends Component {
     };
   }
 
+  deleteComment(comment) {
+    debugger;
+    fetch(`http://localhost:3001/comments/${comment.id}`, {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "whatever-you-want"
+      }
+    })
+      .then(res => res.json())
+      .then(this.props.fetchPostComments(comment.parentId));
+  }
+
   view() {
     const { comment } = this.state;
     return (
@@ -17,6 +32,7 @@ class Comment extends Component {
         <div>Author: {comment.author}</div>
         <div>Body: {comment.body}</div>
         <button onClick={() => this.setState({ edit: true })}>Edit</button>
+        <button onClick={() => this.deleteComment(comment)}>Delete</button>
       </div>
     );
   }
@@ -45,4 +61,4 @@ class Comment extends Component {
   }
 }
 
-export default Comment;
+export default connect(null, { fetchPostComments })(Comment);
