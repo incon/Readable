ffa3b3b10e1f0c9fb30b0f42c0ea9d6bc0b44b76
David Darrell <david.darrell@gmail.com>
2017-11-27 21:46:38 +0800

    Make PostCretePage stateless

--- frontend/src/components/PostCreatePage.js
+++ frontend/src/components/PostCreatePage.js
@@ -2,16 +2,14 @@ import React, { Component } from "react";
 import PostEdit from "./PostEdit";
 import { Link } from "react-router-dom";
 
-class PostCretePage extends Component {
-  render() {
-    return (
-      <div>
-        <Link to="/">Home</Link>
-        <h2>Create a New Post</h2>
-        <PostEdit new={true} history={this.props.history} />
-      </div>
-    );
-  }
+function PostCretePage(props) {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <h2>Create a New Post</h2>
+      <PostEdit new={true} history={this.props.history} />
+    </div>
+  );
 }
 
 export default PostCretePage;
