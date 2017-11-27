8e1c41fe743705ae8bcebbc00f7684682e81dadd
David Darrell <david.darrell@gmail.com>
2017-11-27 21:45:48 +0800

    Make posts stateless

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -5,44 +5,42 @@ import { deletePost } from "../actions/index";
 import { format } from "date-fns";
 import Score from "./Score";
 
-class Post extends Component {
-  removePost(postId) {
-    this.props.deletePost(postId);
-    this.props.history.push(`/`);
-  }
+function removePost(postId) {
+  this.props.deletePost(postId);
+  this.props.history.push(`/`);
+}
 
-  render() {
-    const { post } = this.props;
+function Post(props) {
+  const { post } = props;
 
-    return (
-      <div className="post-list-item">
-        <div className="post-list-item-title">{post.title}</div>
-        <div className="post-list-item-comments">
-          Comments: {post.commentCount}
-        </div>
-        <Score type="posts" data={post} />
-        <div className="post-list-item-body">{post.body}</div>
-        <div>
-          <span className="post-list-item-category">{post.category}</span>
-        </div>
-        <div className="post-list-item-when-by">
-          Posted {format(new Date(post.timestamp), "YYYY-MM-DD HH:mm:ss")} by{" "}
-          {post.author}
-        </div>
-        <div className="post-list-item-footer">
-          {this.props.view && (
-            <Link to={`/${post.category}/${post.id}`}>
-              <button>View</button>
-            </Link>
-          )}
-          <Link to={`/posts/${post.id}/edit`}>
-            <button>Edit</button>
-          </Link>
-          <button onClick={() => this.removePost(post.id)}>Delete</button>
-        </div>
+  return (
+    <div className="post-list-item">
+      <div className="post-list-item-title">{post.title}</div>
+      <div className="post-list-item-comments">
+        Comments: {post.commentCount}
       </div>
-    );
-  }
+      <Score type="posts" data={post} />
+      <div className="post-list-item-body">{post.body}</div>
+      <div>
+        <span className="post-list-item-category">{post.category}</span>
+      </div>
+      <div className="post-list-item-when-by">
+        Posted {format(new Date(post.timestamp), "YYYY-MM-DD HH:mm:ss")} by{" "}
+        {post.author}
+      </div>
+      <div className="post-list-item-footer">
+        {props.view && (
+          <Link to={`/${post.category}/${post.id}`}>
+            <button>View</button>
+          </Link>
+        )}
+        <Link to={`/posts/${post.id}/edit`}>
+          <button>Edit</button>
+        </Link>
+        <button onClick={() => this.removePost(post.id)}>Delete</button>
+      </div>
+    </div>
+  );
 }
 
 export default connect(null, { deletePost })(Post);
